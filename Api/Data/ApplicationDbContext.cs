57cf7f9b8e67509f2b248b9d065ca06cbf941c44
erfan-j <erfan.jahandoost84@gmail.com>
2024-09-04 12:55:53 +0330

    feat: Initial Commit.

---
+++ Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
---
+++ Api/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿namespace Api.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int? StockId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreationTime { get; set; } = DateTime.Now;
+
+        //navigation prop:
+        public Stock? Stock { get; set; }
+    }
+}
